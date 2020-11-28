0f4de34b7a7c839ac6e72f5dac21de69e7adc7cc
QwazyWabbit <gandalf.2004@gmail.com>
2020-11-27 18:01:42 -0800

    Add project files.

---
+++ main.c
@@ -0,0 +1,16 @@
+#include <string.h>
+#include <stdio.h>
+
+typedef float vec_t;
+typedef vec_t vec3_t[3];
+
+int main(void)
+{
+	char* value = { 0 };
+	vec3_t	vec = { 0 };
+
+	if (sscanf_s(value, "%f %f %f", &vec[0], &vec[1], &vec[2]) != 3) {
+		printf("WARNING: Vector field incomplete in %s\n", __func__);
+	}
+	// ... do something else ...
+}
